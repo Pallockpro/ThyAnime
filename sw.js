bb940eb52ec2ccf622f349ba9a80a65046cb6a2f
Rimuru <163799725+Pallockpro@users.noreply.github.com>
2026-06-24 00:42:50 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11194004
+    "zoneId": 11193992
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
